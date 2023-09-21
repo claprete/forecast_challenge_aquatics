ddb6c52170daf7e173f344c58d03b6761002a9d5
Carolyn LaPrete <carolynlaprete@gmail.com>
2023-09-21 15:02:23 -0600

    intall packages

--- starting_script.R
+++ starting_script.R
@@ -1,2 +1,9 @@
 # Creating the project
 
+install.packages('remotes')
+install.packages('tidyverse') # collection of R packages for data manipulation, analysis, and visualisation
+install.packages('lubridate') # working with dates and times
+remotes::install_github('eco4cast/neon4cast')
+
+
+
